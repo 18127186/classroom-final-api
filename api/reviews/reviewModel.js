53ea68aa6f2ae22a9215c81fef2a6288421786a6
nhaatjminh <minhphan.240199@gmail.com>
2022-01-04 17:19:20 +0700

    fix get detail grade review

--- api/reviews/reviewModel.js
+++ api/reviews/reviewModel.js
@@ -8,9 +8,10 @@ exports.getListReviews = (classID) => db.execute(
 )
 
 exports.getReviewDetail = (id) => db.execute(
-    `SELECT * 
-    FROM reviews 
-    WHERE id = ${id}`
+    "SELECT A.id, A.assign_id, B.topic, A.student_id, A.current_grade, A.update_grade, A.done, A.expect_grade, A.explanation" 
+    +  ` FROM reviews as A  
+    JOIN assignments as B ON A.assign_id = B.id 
+    WHERE A.id = ${id}`
 )
 
 exports.updateGrade = (update_grade, id) => db.execute(
